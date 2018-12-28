f064e3b9bc42f784e0367cf72ca7fcab1d60c2cd
Kareem McDavid <kareemmcdavid@hotmail.com>
2018-12-28 03:08:22 -0500

    New Stuff

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ require('dotenv').config()
 
 const fs = require('fs');
 const Discord = require('discord.js');
-const { Pool } = require('pg');
+// const { Pool } = require('pg');
 
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: true
+// });
 
 
 
